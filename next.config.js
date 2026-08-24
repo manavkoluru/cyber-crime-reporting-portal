cf7e3936522dff383e09b4983f09f0dc6c45ce23
manavkoluru <manavkoluru@gmail.com>
2026-08-25 00:03:25 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // Allow build to proceed even with type errors
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
